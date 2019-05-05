713f42490a883cff9c31c41753922a4cd8a4d37f
sabit_huraira <sabitzhabit@gmail.com>
2019-05-05 17:13:45 +0700

    no idea what happen

--- app/src/main/java/com/farifam/siwasis/BlokActivity.kt
+++ app/src/main/java/com/farifam/siwasis/BlokActivity.kt
@@ -1,14 +1,21 @@
 package com.farifam.siwasis
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.farifam.siwasis.adapter.BlokAdapter
-import com.farifam.siwasis.model.Blok
+import com.farifam.siwasis.model.Question
 
 import kotlinx.android.synthetic.main.activity_blok.*
-import kotlinx.android.synthetic.main.content_kewajaran.*
+import java.io.IOException
+import java.io.InputStream
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
+import android.service.autofill.Dataset
+import com.google.gson.reflect.TypeToken
+import java.lang.Exception
+
 
 class BlokActivity : AppCompatActivity() {
 
@@ -17,15 +24,59 @@ class BlokActivity : AppCompatActivity() {
         setContentView(R.layout.activity_blok)
         setSupportActionBar(toolbar)
 
-        val listHeroes = listOf(
-            Question(name = "BLOK I", deskripsi =  "Keterangan Tempat")
-        )
+//        try {
+//            val inputStream: InputStream = assets.open("news_data_file.json")
+//            val inputStreamReader = InputStreamReader(inputStream)
+//            val sb = StringBuilder()
+//            var line: String?
+//            val br = BufferedReader(inputStreamReader)
+//            line = br.readLine()
+//            while (br.readLine() != null) {
+//                sb.append(line)
+//                line = br.readLine()
+//            }
+//            br.close()
+//            Log.d(TAG,sb.toString())
+//        } catch (e:Exception){
+//            Log.d(TAG, e.toString())
+//        }
 
-        val heroesAdapter = BlokAdapter(listHeroes)
+        val listDatas: MutableList<Question> = mutableListOf()
 
-        rec_view.apply {
-            layoutManager = LinearLayoutManager(this@BlokActivity)
-            adapter = heroesAdapter
+        val parser = JsonParser()
+        val jsonData =  inputStreamToString(this.getResources().openRawResource(R.raw.rule))
+
+//        val jsonTree = parser.parse(json_data)
+//        val jsonObject = jsonTree.asJsonObject
+
+//        val blok_data = jsonObject.get("blokvi")
+
+        val gson = Gson()
+        val datasetListType = object : TypeToken<Collection<Question>>() {}.type
+        val datasets = gson.fromJson(jsonData, datasetListType)
+
+//        if(jsonTree.isJsonArray()){
+//            listDatas.add(Question("answer": ))
+//        }
+
+//        val myModel = Gson().fromJson(myJson, MyModel::class.java)
+
+//
+//        val heroesAdapter = BlokAdapter(listDatas)
+//
+//        rec_view.apply {
+//            layoutManager = LinearLayoutManager(this@BlokActivity)
+//            adapter = heroesAdapter
+//        }
+    }
+
+    fun inputStreamToString(inputStream: InputStream): String {
+        try {
+            val bytes = ByteArray(inputStream.available())
+            inputStream.read(bytes, 0, bytes.size)
+            return String(bytes)
+        } catch (e: IOException) {
+            null
         }
     }
 
